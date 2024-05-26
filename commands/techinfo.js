d47b6b0e6ac1b66218e570b572707471b7e2b857
only-nub <lupinienkern@gmail.com>
2024-05-26 20:58:34 +0200

    added tech.json and logic

--- commands/techinfo.js
+++ commands/techinfo.js
@@ -1,4 +1,5 @@
 const { EmbedBuilder } = require('discord.js');
+const techData = require('../data/tech.json');
 
 module.exports = {
     name: 'techinfo',
@@ -6,44 +7,26 @@ module.exports = {
     execute(interaction) {
         let tech = interaction.options.get('technique').value;
         
-        let techName;
-        if (tech === 'moonclip') {
-            techName = 'Moon Clip';
-        } else if (tech === 'cbv') {
-            techName = 'Cap Bounce Vector';
-        } else if (tech === 'mcct') {
-            techName = 'Motion Control Cap Throw';
-        } else if (tech === 'rc') {
-            techName = 'Roll Cancel';
+        let techFull = techData.find(tech.toLowerCase() === abbreviation.toLowerCase()); //Gets the matching entry of the JSON file
+
+        let techName = techFull ? techFull.name : null; // If techFull is false (what gets set when techData didn't find anything) sets to null
+        
+        let techExplanation = techFull ? techFull.description : null;
+        
+        if (techName == null || techExplanation == null ){ //Error Handling
+            techName = "Technique not found"
+            techExplanation = "-"
         };
 
-        let abb;
-        if (tech === 'moonclip') {
-            abb = 'No abbreviation';
-        } else {
-            abb = tech;
-        };
-
-        let explanation;
-        if (tech === 'cbv') {
-            explanation =
-                'A type of vector done by tilting the joystick ~40 degrees left or right after doing a cap bounce.';
-        } else if (tech === 'mcct') {
-            explanation =
-                'A cap throw done with motion controls.';
-        } else if (tech === 'rc') {
-            explanation =
-                `A way to transfer Mario's speed from a roll by tapping X, Y and B in quick succession.`;
-        } else if (tech === 'mooncip') {
-            explanation =
-                'A type of out-of-bounds clip done by jumping and cap throwing against the wall, pushing Mario inside it. Can be done with a backflip, sideflip, triple jump or 2P cap bounce.';
-        };
+        if (tech == "moon clip"){
+            tech = "No Abbreviation"
+        }
 
         let techEmbed = new EmbedBuilder()
             .setTitle(techName)
-            .setDescription(explanation)
+            .setDescription(techExplanation)
             .addFields(
-                { name: '**Abbreviation**', value: abb }
+                { name: '**Abbreviation**', value: tech }
             )
             .setColor(0x3498db);
 
