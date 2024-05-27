cb6333e7c4e8ab9360aac214c75475259828284e
only-nub <lupinienkern@gmail.com>
2024-05-27 20:44:03 +0200

    moon clip

--- commands/register.js
+++ commands/register.js
@@ -25,7 +25,7 @@ const commands = [
                     },
                     {
                         name: 'Moon Clip',
-                        value: 'moonclip',
+                        value: 'moon clip',
                     },
                     {
                         name: 'Roll Cancel',
