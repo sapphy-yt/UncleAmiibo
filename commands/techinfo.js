7feaf37277482bfdd7e474f83b43499b4513906a
sapphy-yt <sapphy.terminus@gmail.com>
2024-05-27 20:40:33 +0200

    ;

--- commands/techinfo.js
+++ commands/techinfo.js
@@ -20,7 +20,7 @@ module.exports = {
 
         if (tech == "moon clip") {
             tech = "No Abbreviation"
-        }
+        };
 
         let techEmbed = new EmbedBuilder()
             .setTitle(techName)
